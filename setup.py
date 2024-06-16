660816720903d9343ddcbf379a902427a5d29ece
lokesh-repaka <lokeshrepaka4@gmail.com>
2024-06-16 11:17:46 +0530

    added requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Lokesh",
+    author_email="lokeshrepaka4@gmail.com",
+    packages=find_packages()
+)
